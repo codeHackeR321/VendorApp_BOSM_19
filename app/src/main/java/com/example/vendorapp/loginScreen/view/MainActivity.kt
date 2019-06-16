48afdc3b18270a500d136f82e892633b0b3ebed9
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-06-16 10:49:10 +0530

    Create Notification Channels on App Startup

--- app/src/main/java/com/example/vendorapp/loginScreen/view/MainActivity.kt
+++ app/src/main/java/com/example/vendorapp/loginScreen/view/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.vendorapp.loginScreen.view
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vendorapp.R
@@ -9,5 +12,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeApp()
+    }
+
+    /**
+     * This method is used for the initial setup of the notification channels
+     * If the notification chanel already exists, no action is taken, and hence it is safe to call this method every time the app starts
+     * */
+    fun setupNotificationChannel(){
+        // Notification Channels are only available for Oreo(Api Level 26) and onwards
+        // Since support libraries don't have a library for setting up notification channels, this check is necessary
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            val newOrderChanel = NotificationChannel("NewOrder" , getString(R.string.chanel_name_newOrder) , NotificationManager.IMPORTANCE_HIGH)
+            newOrderChanel.description = getString(R.string.chanel_desc_newOrder)
+            newOrderChanel.canBypassDnd()
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(newOrderChanel)
+        }
+    }
+
+    /**
+     * It is the job of this function to perform all the one-time setups that are needed to be done when the app starts
+     * for the first time
+     * It sets up notification Channel
+     * */
+    fun initializeApp(){
+        setupNotificationChannel()
     }
 }
