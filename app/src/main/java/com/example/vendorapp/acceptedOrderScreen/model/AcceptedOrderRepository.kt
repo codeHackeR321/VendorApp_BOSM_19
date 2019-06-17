6060bca5a0d2d84c4a329e0fc9221a307c7f495d
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2019-06-18 01:22:07 +0530

    Rx implemented in retrofit instance and interface plus minor changes

--- app/src/main/java/com/example/vendorapp/acceptedOrderScreen/model/AcceptedOrderRepository.kt
+++ app/src/main/java/com/example/vendorapp/acceptedOrderScreen/model/AcceptedOrderRepository.kt
@@ -7,15 +7,18 @@ import com.example.vendorapp.dataClasses.roomClasses.OrdersData
 import com.example.vendorapp.singletonObjects.RetrofitInstance
 import com.example.vendorapp.singletonObjects.VendorDatabase
 import io.reactivex.CompletableObserver
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AcceptedOrderRepository(application: Application) {
 
     private val acceptedOrderDao: AcceptedOrderDao
-    private val orderApiCall: Call<List<OrdersPojo>>
+    private val orderApiCall: Observable<List<OrdersPojo>>
 
     init {
 
@@ -25,10 +28,15 @@ class AcceptedOrderRepository(application: Application) {
         orderApiCall = RetrofitInstance.getRetroInstance().orders
     }
 
-    val orders = acceptedOrderDao.getOrders().subscribeOn(Schedulers.io())
+    val ordersRoom = acceptedOrderDao.getOrders().subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
-        .subscribe {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        .subscribe{
+
         }
+
+    val ordersApi = orderApiCall.subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .map {  }
+        .subscribe()
 
 }
--- app/src/main/java/com/example/vendorapp/menu/model/MenuRepository.kt
+++ app/src/main/java/com/example/vendorapp/menu/model/MenuRepository.kt
@@ -5,14 +5,17 @@ import com.example.vendorapp.dataClasses.retroClasses.MenuPojo
 import com.example.vendorapp.menu.model.room.MenuDao
 import com.example.vendorapp.singletonObjects.RetrofitInstance
 import com.example.vendorapp.singletonObjects.VendorDatabase
+import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MenuRepository (application: Application){
 
     private val menuDao: MenuDao
-    private val menuApiCall: Call<MenuPojo>
+    private val menuApiCall: Observable<List<MenuPojo>>
 
     init {
 
@@ -22,9 +25,14 @@ class MenuRepository (application: Application){
         menuApiCall = RetrofitInstance.getRetroInstance().menu
     }
 
-    val menu = menuDao.getMenu().subscribeOn(Schedulers.io())
+    val menuListRoom = menuDao.getMenu().subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe{
 
         }
+
+    val menuApi = menuApiCall.subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .map { }
+        .subscribe()
 }
