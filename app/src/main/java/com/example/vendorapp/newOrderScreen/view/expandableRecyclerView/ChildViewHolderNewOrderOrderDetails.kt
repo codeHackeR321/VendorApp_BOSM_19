9580ae04c5f9ade3cfd977c83fa9812fd5264ef3
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-06-17 14:16:22 +0530

    Add Basic classes for expandable Recycler View

---
+++ app/src/main/java/com/example/vendorapp/newOrderScreen/view/expandableRecyclerView/ChildViewHolderNewOrderOrderDetails.kt
@@ -0,0 +1,10 @@
+package com.example.vendorapp.newOrderScreen.view.expandableRecyclerView
+
+import android.view.View
+import com.thoughtbot.expandablerecyclerview.viewholders.ChildViewHolder
+
+class ChildViewHolderNewOrderOrderDetails(itemView : View?) : ChildViewHolder(itemView){
+
+
+
+}
---
+++ app/src/main/java/com/example/vendorapp/newOrderScreen/view/expandableRecyclerView/GroupViewHolderNewOrderCompactMenu.kt
@@ -0,0 +1,10 @@
+package com.example.vendorapp.newOrderScreen.view.expandableRecyclerView
+
+import android.view.View
+import com.thoughtbot.expandablerecyclerview.viewholders.GroupViewHolder
+
+class GroupViewHolderNewOrderCompactMenu(itemView : View?) :GroupViewHolder(itemView){
+
+
+
+}
