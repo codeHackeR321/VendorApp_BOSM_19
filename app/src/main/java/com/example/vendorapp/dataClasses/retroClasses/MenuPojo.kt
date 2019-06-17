c48ef631017f80faa32cb41faba9c7b54c45206a
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2019-06-17 17:09:41 +0530

    MenuPojo added

---
+++ app/src/main/java/com/example/vendorapp/dataClasses/retroClasses/MenuPojo.kt
@@ -0,0 +1,12 @@
+package com.example.vendorapp.dataClasses.retroClasses
+
+data class MenuPojo (
+
+    var itemId: String,
+
+    var name: String,
+
+    var price: String,
+
+    var status: String
+)
