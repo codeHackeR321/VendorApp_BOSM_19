24a89d18d2679d5744dff139ff31c89bb297682d
99suyashsoni <45849297+99suyashsoni@users.noreply.github.com>
2019-06-17 21:00:50 +0530

    Menu and Accepted Dao

--- app/src/main/java/com/example/vendorapp/acceptedOrderScreen/model/room/AcceptedOrderDao.kt
+++ app/src/main/java/com/example/vendorapp/acceptedOrderScreen/model/room/AcceptedOrderDao.kt
@@ -1,8 +1,16 @@
 package com.example.vendorapp.acceptedOrderScreen.model.room
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.vendorapp.dataClasses.roomClasses.OrdersData
 
 
 @Dao
 interface AcceptedOrderDao {
+
+    // All orders with accepted or ready status
+    @Query("SELECT * FROM orders_table WHERE status = 'accepted' OR status = 'ready' ")
+    fun getOrders(): LiveData<List<OrdersData>>
+
 }
--- app/src/main/java/com/example/vendorapp/menu/model/room/MenuDao.kt
+++ app/src/main/java/com/example/vendorapp/menu/model/room/MenuDao.kt
@@ -1,7 +1,21 @@
 package com.example.vendorapp.menu.model.room
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.vendorapp.dataClasses.roomClasses.MenuItemData
 
 @Dao
 interface MenuDao {
+
+    @Query("SELECT * FROM menu_table")
+    fun getMenu(): LiveData<List<MenuItemData>>
+
+    @Query("DELETE FROM menu_table ")
+    fun deleteAll()
+
+    @Insert
+    fun insertMenu(vararg menuItemData: MenuItemData)
+
 }
