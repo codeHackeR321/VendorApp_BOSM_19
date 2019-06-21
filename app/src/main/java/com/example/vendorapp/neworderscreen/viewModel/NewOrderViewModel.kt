c73bf3ef5a7d0b118d6418df0fe81b50ed60e6f9
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-06-22 00:32:32 +0530

    Update NewOrderViewModel

--- app/src/main/java/com/example/vendorapp/neworderscreen/viewModel/NewOrderViewModel.kt
+++ app/src/main/java/com/example/vendorapp/neworderscreen/viewModel/NewOrderViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vendorapp.neworderscreen.view.ModifiedOrdersDataClass
+import com.example.vendorapp.shared.dataclasses.roomClasses.ItemData
 import com.example.vendorapp.shared.expandableRecyclerView.ChildDataClass
 import com.example.vendorapp.shared.singletonobjects.repositories.NewOrderRepositoryInstance
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -21,24 +22,22 @@ class NewOrderViewModel(context : Context) : ViewModel(){
     @SuppressLint("CheckResult")
     fun getNewOrders() {
 
-        var list = emptyList<ModifiedOrdersDataClass>()
+        var ordersList = emptyList<ModifiedOrdersDataClass>()
         newOrderRepo.getNewOrdersList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-            .doOnNext { orderList ->
-                orderList.forEach { order ->
-                    //                        var childList = emptyList<ChildDataClass>()
-//                        order.items.forEach {
-//                            childList.plus(ChildDataClass(itemId = it.itemId , itemName = "Default Name" , price = it.price , quantity = it.quantity))
-//                        }
-//                        list.plus(ModifiedOrdersDataClass(timestamp = order.timestamp.toString() , totalAmount = order.totalAmount , status = order.status , otp = order.otp , items = childList , orderId = order.orderId))
-//                    }
-//                    (orders as MutableLiveData<List<ModifiedOrdersDataClass>>).postValue(list)
-//                }
-//
-//    }
-
-
-
+            .doOnNext {orderList ->
+                orderList.forEach {order ->
+                    var itemsList = emptyList<ChildDataClass>()
+                    newOrderRepo.getItemsOfOrder(order.orderId).observeOn(AndroidSchedulers.mainThread()).
+                        doOnNext {
+                            it.forEach {
+                                itemsList = itemsList.plus(ChildDataClass("Item Name" , it.price , it.quantity , it.itemId))
+                            }
+                        }.
+                        doOnComplete {
+                            ordersList = ordersList.plus(ModifiedOrdersDataClass(order.orderId , order.status , order.timestamp.toString() , order.otp , order.totalAmount , itemsList))
+                    }
                 }
+                (orders as MutableLiveData<List<ModifiedOrdersDataClass>>).postValue(ordersList)
             }
     }
 
