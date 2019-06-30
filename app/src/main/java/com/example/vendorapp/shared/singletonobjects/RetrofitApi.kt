515b22050ada7d2e6a3e3d7c9c67c60995f1c27f
ishita Aggarwal <e.agrawal03@gmail.com>
2019-06-30 13:00:47 +0530

    Added functionality for updating order status

--- app/src/main/java/com/example/vendorapp/shared/singletonobjects/RetrofitApi.kt
+++ app/src/main/java/com/example/vendorapp/shared/singletonobjects/RetrofitApi.kt
@@ -3,8 +3,13 @@ package com.example.vendorapp.shared.singletonobjects
 import com.example.vendorapp.shared.dataclasses.retroClasses.EarningsPojo
 import com.example.vendorapp.shared.dataclasses.retroClasses.MenuPojo
 import com.example.vendorapp.shared.dataclasses.retroClasses.OrdersPojo
+import com.google.gson.JsonObject
+import io.reactivex.Completable
 import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface RetrofitApi {
@@ -18,5 +23,7 @@ interface RetrofitApi {
     @get:GET("earnings")
     val earnings: Single<EarningsPojo>
 
+    @POST("orders")
+    fun updateStatus(@Body body: JsonObject):Single<Response<Unit>>
     //Add post when api available
 }
